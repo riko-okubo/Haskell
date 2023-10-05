faa29eecdfe4135ecbcf83a46a65ebfff5c8cd7e
riko-okubo <12riko14@gmail.com>
2023-10-05 15:21:10 +0900

    2.1-2.2

---
+++ sumfs.hs
@@ -0,0 +1,11 @@
+-- 高階関数
+
+-- sumfsは関数と2つの整数値を引数に取り、
+-- 第1引数の関数を第２、第3引数の整数値に
+-- それぞれ適用した結果の和を返す高階関数
+
+sumfs :: (Int -> Int) -> Int -> Int -> Int
+sumfs f x y = f x + f y
+
+square :: Int -> Int
+square \n -> n * n        --ここがラムダ式だから、sumfsで関数として使える
---
+++ sumsquares.hs
@@ -0,0 +1,20 @@
+bar :: Int
+bar = y * z             -- このbarは大域的
+    where y = z * 2     -- whereにより局所的な定義を与える
+          z = 8         -- 局所的な環境にはy,zがある
+
+zoo :: Int
+zoo = let bar = 10 in bar * 5 -- letにより局所的な定義を与える
+
+sumsquares :: Int -> Int -> Int
+sumsquares x y = sq x + sq y
+    where sq n = n * n  --sq : 局所的に定義された関数
+
+-- sumsquares x y =  let sq n = n * n
+--    in sq x + sq y
+-- という書き方でも良い（letを用いた書き方）
+
+
+-- 部分適用を用いる書き方
+-- sumsquares :: Int -> Int -> Int
+-- sumsquares = sumfs square 
